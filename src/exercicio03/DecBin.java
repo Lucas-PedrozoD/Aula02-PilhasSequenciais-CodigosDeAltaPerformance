defaf761836a5e9e4cee0b7b1c355fb822d5fbec
Lucas Pedrozo Dias <lucas.fac.pedrozo@gmail.com>
2026-03-30 19:33:48 -0300

    Commit exercicios

--- src/exercicio03/DecBin.java
+++ src/exercicio03/DecBin.java
@@ -5,15 +5,28 @@ import java.util.Scanner;
 public class DecBin {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int valorEmDecimal, resto;
+        int valorEmDecimal, cont = 0, duplicat;
+
         System.out.print("Valor em decimal: ");
         valorEmDecimal = sc.nextInt();
+        duplicat = valorEmDecimal;
 
-        System.out.println("Valor em binario: ");
+        while(duplicat != 0){
+            duplicat /= 2;
+            cont ++;
+        }
+
+        int [] resto = new int[cont];
+
+        int j = cont - 1;
         while(valorEmDecimal != 0){
-            resto = valorEmDecimal % 2;
+            resto[j] = valorEmDecimal % 2;
             valorEmDecimal /= 2;
-            System.out.print(resto);
+            j--;
+        }
+        System.out.println("Valor em binario: ");
+        for (int i = 0; i < resto.length; i++){
+            System.out.print(resto[i]);
         }
     }
 }
